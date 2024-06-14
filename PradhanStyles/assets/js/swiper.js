7138e4ad74ea06c3fdd995b70f9fc5ff05cef552
Pratham Saraiya <81284893+pratham016@users.noreply.github.com>
2024-06-14 23:40:43 +0530

    Update swiper.js

--- PradhanStyles/assets/js/swiper.js
+++ PradhanStyles/assets/js/swiper.js
@@ -1,14 +1,3 @@
-/**
- * Swiper 4.3.3
- * Most modern mobile touch slider and framework with hardware accelerated transitions
- * http://www.idangero.us/swiper/
- *
- * Copyright 2014-2018 Vladimir Kharlampidi
- *
- * Released under the MIT License
- *
- * Released on: June 5, 2018
- */
 
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
